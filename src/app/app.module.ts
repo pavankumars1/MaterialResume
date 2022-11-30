c081f31b8bf947891b86ecf83e876efc3ed95cab
pavankumars1 <119483491+pavankumars1@users.noreply.github.com>
2022-12-01 00:15:54 +0530

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { MaterialModule } from './material/material.module';
 
@@ -7,7 +8,12 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, MaterialModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+  ],
   declarations: [AppComponent, HelloComponent],
   bootstrap: [AppComponent],
 })
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,8 +3,14 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
 
-const matComponents = [MatToolbarModule, MatButtonModule, MatCardModule];
+const matComponents = [
+  MatToolbarModule,
+  MatButtonModule,
+  MatCardModule,
+  MatMenuModule,
+];
 @NgModule({
   imports: [CommonModule, matComponents],
   exports: [matComponents],
