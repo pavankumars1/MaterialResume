42ced162f5b360d5fa2c9a17aa8a12aad9f2a059
pavankumars1 <119483491+pavankumars1@users.noreply.github.com>
2023-03-10 12:27:44 +0530

    Updates in this commit ______________________  1. Angular material expansion module has been used. 2. CSS has been updated 3. Font Awesome was used and index.html was updated with minified css link

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -4,12 +4,16 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
 
 const matComponents = [
   MatToolbarModule,
   MatButtonModule,
   MatCardModule,
   MatMenuModule,
+  MatExpansionModule,
+  MatIconModule
 ];
 @NgModule({
   imports: [CommonModule, matComponents],
